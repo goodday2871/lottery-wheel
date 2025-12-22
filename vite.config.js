cb4fdee8dae47e3417cd6e3fe877663497863370
danielwu <danielwu@staff.ruten.com.tw>
2025-12-22 12:31:12 +0800

    設置 GitHub Pages 部署
    - 將構建輸出目錄從 dist 改為 docs
    - 添加 base 路徑設定確保相對路徑正常運作
    - docs/index.html 為單一 HTML 檔案，可離線使用也可部署

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,9 @@ export default defineConfig({
     vue(),
     viteSingleFile()
   ],
+  base: './', // 使用相對路徑，確保在任何環境下都能運作
   build: {
+    outDir: 'docs', // 輸出到 docs 目錄供 GitHub Pages 使用
     target: 'esnext',
     assetsInlineLimit: 100000000, // 確保所有資源都被內聯
     chunkSizeWarningLimit: 100000000,
